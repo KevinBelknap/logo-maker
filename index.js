1ccf0bd838fc995152ca9c6bd0af5c52d220c3f5
Kevin Belknap <kevin.belknap@pepsico.com>
2023-06-12 23:07:38 -0500

    baseline files and code

---
+++ index.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+const inquirer = require("inquirer");
+const {Circle, Square, Triangle} = require("./lib/shapes");
+
+// Defines array of 'questions' using the 'inquirer' library with the following questions.
+// Each question is an object that specifies the properties of TEXT, TEXT COLOR, SHAPE COLOR, and Pixel Image.
+const questions = [
+    {
+        type: "input",
+        name: "text",
+        message: "TEXT: Enter up to (3) Characters:",
+    },
+    {
+        type: "input",
+        name: "text-color",
+        message: "TEXT COLOR: Enter a color keyword (OR a hexadecimal number):",
+    },
+    {
+        type: "input",
+        name: "shape",
+        message: "SHAPE COLOR: Enter a color keyword (OR a hexadecimal number):",
+    },
+    {
+        type: "list",
+        name: "pixel-image",
+        message: "Choose which Pixel Image you would like?",
+        choices: ["Circle", "Square", "Triangle"],
+    },
+];
+
+// Function to write data to file
+function writeToFile(fileName, data) {
+	console.log("Writing [" + data + "] to file [" + fileName + "]")
+    filesystem.writeFile(fileName, data, function (err) {
+        if (err) {
+            return console.log(err);
+        }
+        console.log("Congratulations, you have Generated a logo.svg!");
+    });
+}
